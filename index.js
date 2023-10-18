ba875dce035c7e587786267cf9cb6fcb0fef4a8a
wz-adrian <adrian@Whitezip.com>
2023-10-18 13:05:01 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Pragmatic from './internal-js/pragmatic';
+import Ezugi from './internal-js/ezugi';
 
 class CGWidget {
     constructor() {
@@ -7,5 +8,6 @@ class CGWidget {
 
     connectPragmaticAPI() {
         new Pragmatic(1,2);
+        new Ezugi(1,0);
     }
 }
