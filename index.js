86ba5c312eb1d5b093406744590d6852232da3ea
Adrian Maldonado Bacigalupo <adrian272566@hotmail.es>
2023-12-04 15:10:35 +0000

    update

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+import Ezugi from './internal-js/ezugi';
 import Pragmatic from "./internal-js/pragmatic";
 
 class CGWidget {
     constructor() {
+        this.ezugi = null;
         this.pragmatic = null;
     }
 
     connectPragmaticAPI(operator, currency = 'EUR', options = null) {
         this.pragmatic = new Pragmatic(operator, currency, options);
+    }
+
+    connectEzugiAPI() {
+        this.ezugi = new Ezugi("ezeplayz-site");
     }
 }
 
